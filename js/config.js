c40b2d02de3c7d38f086518eaac16951f8ce3686
frontc <fangchao@lefer.cn>
2017-08-07 14:07:33 +0800

    修改默认项

--- js/config.js
+++ js/config.js
@@ -1,9 +1,9 @@
 var CONFIG = {
 	AVAILABLE_DBS: ["mysql", "sqlite", "web2py", "mssql", "postgresql", "oracle", "sqlalchemy", "vfp9", "cubrid", "web2py"],
-	DEFAULT_DB: "mysql",
+	DEFAULT_DB: "mssql",
 
 	AVAILABLE_LOCALES: ["ar", "cs", "de", "el", "en", "eo", "es", "fr", "hu", "it", "ja", "ko", "nl", "pl", "pt_BR", "ro", "ru", "sv", "zh"],
-	DEFAULT_LOCALE: "en",
+	DEFAULT_LOCALE: "zh",
 	
 	AVAILABLE_BACKENDS: ["php-mysql", "php-blank", "php-file", "php-sqlite", "php-mysql+file", "php-postgresql", "php-pdo", "perl-file", "php-cubrid", "asp-file", "web2py"],
 	DEFAULT_BACKEND: ["php-mysql"],
